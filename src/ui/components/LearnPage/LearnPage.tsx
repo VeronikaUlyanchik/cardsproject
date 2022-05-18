dc1b1dffe563b225b3aee3830b2b509d0eb6c22f
Veronika Ulyanchik <veronikaullll1998@gmail.com>
2022-05-18 16:10:44 +0300

    learn page

--- src/ui/components/LearnPage/LearnPage.tsx
+++ src/ui/components/LearnPage/LearnPage.tsx
@@ -6,6 +6,7 @@ export const LearnPage = () => {
 
 
     return (
+        <></>
         // <ContentWrapper width={"65%"} height={"85%"} flex={"flex"} direction={"column"}>
         //
         // </ContentWrapper>
